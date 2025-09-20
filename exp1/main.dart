e46158706b43ad152d5210702af04eeb6dbaeb51
mdsana_14 <mailmdsana@gmail>
2025-09-20 21:47:34 +0530

    Initial commit:Added exp1

---
+++ exp1/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("Hello, Dart!");  
+  int rno = 538;                  
+  String name = "Sana";   
+  bool isStudent = true;  
+  print("My name is $name");
+  print("My Rollno is $rno");
+  print("Am I a student? $isStudent");
+}
